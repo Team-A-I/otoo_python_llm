e3769016fbc05a1cc44c8992ddb3492ae5be0e02
배정현\bjh73 <bae4445@gmail.com>
2024-07-20 15:25:43 +0900

    [Feature] stt개발중- 음성업로드 완료

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 import logging
-import os
+
 from api.emotion_report_router import router as emotion_report_router
 from api.chatbot_router import router as chatbot_router
 from api.analysis_router import router as analysis_router
@@ -9,12 +9,14 @@ from api.ocr_router import router as ocr_router
 from api.stt_router import router as stt_router
 from dotenv import load_dotenv
 
+
 load_dotenv()
 # 로그 설정
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
 
 origins = [
     "https://fastapi.otoo.kr",
