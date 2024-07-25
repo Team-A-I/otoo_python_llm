c96f46b08daa3f2b6d89e211a9a28ef5bd2bef65
khe8895 <khe8895@gachon.ac.kr>
2024-07-25 20:41:35 +0900

    [Feature]색깔, 크기 제한없이 캡쳐본 인식하기

--- app/models/image_processor.py
+++ app/models/image_processor.py
@@ -7,108 +7,107 @@ class ImageProcessor:
     def __init__(self):
         self.ocr = MyPaddleOCR()
 
-    def check_guideline(self, point, top_left, bottom_right):
-        x, y = point
-        x_min, y_min = top_left
-        x_max, y_max = bottom_right
-        return x_min <= x <= x_max and y_min <= y <= y_max
+    def adjust_contrast(self, image):
+        lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=6.0, tileGridSize=(9, 9))
+        cl = clahe.apply(l)
+        limg = cv2.merge((cl, a, b))
+        return cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
 
-    def get_bounding_boxes(self, contours, margin=150, direction='left'):
-        bounding_boxes = []
+    def find_contours(self, image):
+        kernel = np.ones((2, 2), np.uint8)
+        morphed = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=1)
+        dilated = cv2.dilate(morphed, kernel, iterations=2)
+        eroded = cv2.erode(dilated, kernel, iterations=1)
+        edged = cv2.Canny(eroded, 30, 150)
+        contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
+
+    def get_bounding_boxes(self, contours, height, width, margin=135):
+        contour_coords = []
+        min_contour_width = 50
+        min_contour_height = 30
+
         for contour in contours:
             x, y, w, h = cv2.boundingRect(contour)
-            if direction == 'left':
-                x = max(0, x - margin)
-            w += margin
-            h += 10
-            bounding_boxes.append((x, y, x + w, y + h))
-        return bounding_boxes
+            if w >= min_contour_width and h >= min_contour_height:
+                contour_coords.append((x, y, w, h))
+        
+        left_boxes = []
+        right_boxes = []
+        for x, y, w, h in contour_coords:
+            contour_center_x = x + w // 2
+            distance_to_left = contour_center_x
+            distance_to_right = width - contour_center_x
 
-    def extract_text_from_boxes(self, boxes, ocr_results):
-        text_list = []
-        for box in boxes[::-1]:
-            result = []
-            for points, text in ocr_results:
-                all_inrange = all(self.check_guideline(point, box[1], box[3]) for point in points)
-                if all_inrange:
-                    result.append(text[0])
-            text_list.append(" ".join(result))
-        return text_list
+            if distance_to_left < distance_to_right:
+                new_w = min(w + margin, width - x)
+                left_boxes.append((x, y, x + new_w, y + h))
+            else:
+                new_x = max(0, x - margin)
+                new_w = min(w + margin, width - new_x)
+                right_boxes.append((new_x, y, new_x + new_w, y + h))
+
+        return left_boxes, right_boxes
 
     def draw_bounding_boxes(self, image, boxes, color):
         for box in boxes:
             x1, y1, x2, y2 = box
             cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
 
+    def extract_text_from_boxes(self, boxes, ocr_results):
+        text_list = []
+        for box in boxes:
+            result = []
+            for points, text in ocr_results:
+                x1, y1, x2, y2 = box
+                all_inrange = all(x1 <= px <= x2 and y1 <= py <= y2 for px, py in points)
+                if all_inrange:
+                    result.append(text[0])
+            text_list.append(" ".join(result))
+        return text_list
+
     async def process_image(self, img_bytes):
         try:
-            # Use the existing OCR object
             ocr_result = await self.ocr.run_ocr(img_bytes)
             if ocr_result is None:
                 raise Exception("Failed to perform OCR")
 
-            # Load and preprocess image
             image = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+            height, width = image.shape[:2]
 
-            # Convert to HSV and create masks
-            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            yellow_lower = np.array([25, 150, 150])
-            yellow_upper = np.array([30, 255, 255])
-            white_lower = np.array([0, 0, 250])
-            white_upper = np.array([180, 5, 255])
+            adjusted_image = self.adjust_contrast(image)
+            gray = cv2.cvtColor(adjusted_image, cv2.COLOR_BGR2GRAY)
+            contours = self.find_contours(gray)
 
-            mask_yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
-            mask_white = cv2.inRange(hsv, white_lower, white_upper)
+            left_boxes, right_boxes = self.get_bounding_boxes(contours, height, width)
 
-            # Find contours
-            contours_yellow, _ = cv2.findContours(mask_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contours_white, _ = cv2.findContours(mask_white, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            if not contours_yellow and not contours_white:
-                raise Exception("No yellow or white contours found")
-
-            # Get bounding boxes
-            yellow_boxes = self.get_bounding_boxes(contours_yellow, direction='left')
-            white_boxes = self.get_bounding_boxes(contours_white, direction='right')
-
-            if not yellow_boxes and not white_boxes:
-                raise Exception("No yellow or white bounding boxes found")
-
-            # Draw bounding boxes
             image_with_boxes = image.copy()
-            self.draw_bounding_boxes(image_with_boxes, yellow_boxes, (0, 0, 255))
-            self.draw_bounding_boxes(image_with_boxes, white_boxes, (100, 36, 100))
+            self.draw_bounding_boxes(image_with_boxes, left_boxes, (255, 0, 0))
+            self.draw_bounding_boxes(image_with_boxes, right_boxes, (0, 0, 255))
 
-            # Extract text from boxes
-            yellow_coords = [["yellow", (x1, y1), (x2, y1), (x2, y2), (x1, y2)] for x1, y1, x2, y2 in yellow_boxes]
-            white_coords = [["white", (x1, y1), (x2, y1), (x2, y2), (x1, y2)] for x1, y1, x2, y2 in white_boxes]
+            left_text = self.extract_text_from_boxes(left_boxes, ocr_result)
+            right_text = self.extract_text_from_boxes(right_boxes, ocr_result)
 
-            yellow_text = self.extract_text_from_boxes(yellow_coords, ocr_result)
-            white_text = self.extract_text_from_boxes(white_coords, ocr_result)
-
-            if not yellow_text and not white_text:
-                raise Exception("No text found in yellow or white boxes")
-
-            return image_with_boxes, white_text, yellow_text
+            return image_with_boxes, left_text, right_text
         except Exception as e:
             print(f"Error processing image: {e}")
             return None, [], []
 
     async def process_images(self, image_files):
-        all_white_text = []
-        all_yellow_text = []
+        all_left_text = []
+        all_right_text = []
 
         for image_file in image_files:
             img_bytes = await image_file.read()
-            image_with_boxes, white_text, yellow_text = await self.process_image(img_bytes)
-            
+            image_with_boxes, left_text, right_text = await self.process_image(img_bytes)
+
             if image_with_boxes is not None:
-                all_white_text.extend(white_text)
-                all_yellow_text.extend(yellow_text)
-            else:
-                raise ValueError("No valid image with boxes found")
+                all_left_text.extend(left_text)
+                all_right_text.extend(right_text)
 
-        all_white_text = [re.sub(r'크{2,}', 'ㅋㅋ', text) for text in all_white_text if text.strip()]
-        all_yellow_text = [re.sub(r'크{2,}', 'ㅋㅋ', text) for text in all_yellow_text if text.strip()]
+        all_left_text = [re.sub(r'크{2,}', 'ㅋㅋ', text) for text in all_left_text if text.strip()]
+        all_right_text = [re.sub(r'크{2,}', 'ㅋㅋ', text) for text in all_right_text if text.strip()]
 
-        return all_white_text, all_yellow_text
+        return all_left_text, all_right_text
