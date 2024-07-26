3309b1e5d2c01d4910d1a5be05bde4ba77be6b35
hyunseok <gustjrl09@naver.com>
2024-07-26 17:36:38 +0900

    [Feature]어드민임베딩값수정기능

--- app/models/models.py
+++ app/models/models.py
@@ -11,9 +11,21 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from pymongo import MongoClient
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
+
+# class Document:
+#     def __init__(self, page_content, metadata):
+#         self.page_content = page_content
+#         self.metadata = metadata
+
+#     def __repr__(self):
+#         return f"Document(page_content='{self.page_content[:30]}...', metadata={self.metadata})"
 
 class EmotionReportModel:
     def __init__(self, api_key):
@@ -58,24 +70,76 @@ class QnaModel:
         messages = messages_request.messages
         
         try:
+            # MongoDB 클라이언트 초기화 및 컬렉션 접근
+            mongo = MongoClient(os.getenv('MONGO_CLIENT_URL'))
+            db = mongo[os.getenv('MONGO_DB')]
+            collection = db[os.getenv('MONGO_COLLENTION_QNA')]
+
+            documents = list(collection.find({}, {'_id': 0, 'content': 1, 'embedding': 1}))
+            documents = [Document(page_content=doc["content"], embedding=doc["embedding"]) for doc in documents]
+            # FAISS에 임베딩 데이터 로드
             embeddings = OpenAIEmbeddings()
-            loaded_vectorstore = FAISS.load_local("faiss/qna_index", embeddings, allow_dangerous_deserialization=True)
-            retriever = loaded_vectorstore.as_retriever()
+            vectorstore = FAISS.from_documents(
+                documents=documents,
+                embedding=embeddings
+)
+
+            # 검색기 생성
+            retriever = vectorstore.as_retriever()
+       
+            # LLM 및 프롬프트 템플릿 초기화
             llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
             prompt = PromptTemplate.from_template(qna_prompt)
-            # 체인 설정
+           
+            # 체인 설정: 검색기, LLM, 출력 파서
             chain = (
                 {"context": retriever, "question": RunnablePassthrough()}
                 | prompt
                 | llm
                 | StrOutputParser()
             )
+            
+            # 체인에 질문 입력 및 응답 생성
             question = messages
             response = chain.invoke(question)
         except Exception as e:
-            raise HTTPException(status_code=500, detail=f"Error calling OpenAI API: {str(e)}")
+            raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
 
         return response
+    
+class QnAEditorModel:
+    def __init__(self, api_key):
+        self.client = AsyncOpenAI(api_key=api_key)
+    
+    async def initialize(self):
+        self.model_name = await get_model_name()
+    
+    async def generate_qna_edit_response(self, messages_request):
+        await self.initialize()
+        messages = messages_request.messages
+
+        try:
+            client = MongoClient(os.getenv('MONGO_CLIENT_URL'))
+            db = client[os.getenv('MONGO_DB')]
+            collection = db[os.getenv('MONGO_COLLENTION_QNA')]
+
+            collection.delete_many({})
+
+            text_stream = [Document(page_content= messages, metadata= {'source': 'QnA'})]
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+            split_documents = text_splitter.split_documents(text_stream)
+
+            # 임베딩 생성
+            embeddings = OpenAIEmbeddings()
+            embedded_docs = [{"content": doc.page_content, "embedding": embeddings.embed_documents([doc.page_content])[0]} for doc in split_documents]
+
+            # MongoDB에 저장
+            collection.insert_many(embedded_docs)
+                        
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error calling OpenAI API: {str(e)}")
+
+        return HTTPException(status_code=200, detail="QnA 데이터베이스 업데이트 완료")
 
 class ChatbotModel:
     def __init__(self, api_key):
@@ -223,6 +287,7 @@ chatbot_model = ChatbotModel(api_key=api_key)
 analysis_model = AnalysisModel(api_key=api_key)
 ocr_model = OcrModel(api_key=api_key)
 qna_model = QnaModel(api_key=api_key)
+qna_editor_model = QnAEditorModel(api_key=api_key)
 
 def get_emotion_report_model():
     return emotion_report_model
@@ -243,5 +308,8 @@ def get_ocr_model():
 
 def get_qna_model():
     return qna_model
+
+def get_qna_editor_model():
+    return qna_editor_model
     
 
